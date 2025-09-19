cd1d93b2a3b686457994f81a67e1698737b3de73
JaylaCraddock <jaylascraddock@gmail.com>
2025-09-19 18:08:25 -0500

    Update Bookstore.java

--- Bookstore/Bookstore/src/Bookstore.java
+++ Bookstore/Bookstore/src/Bookstore.java
@@ -46,6 +46,86 @@ public static void bookSearch() {
     }
 }
 
+//Name: Jayla Craddock
+//Date: 9/19/25
+//Assignment: Team Assignment Part 2
+import java.util.ArrayList; // Import the arraylist class
+import java.util.Scanner; //Import the scanner class
+
+public class Books {
+    public static void main(String[] args){
+    
+    //Array List for books.
+    ArrayList<String> books = new ArrayList<>();
+    Scanner keyboard = new Scanner(System.in);
+    
+    //Input from user.
+    String input;
+    
+    //Stop to help break out of loop.
+    String str1 = "stop";
+
+//Introduce the user to the database.
+System.out.println("Welcome to the Book Database! Enter a book title into the database. To stop type the word stop. ");
+
+//While loop for adding book titles.
+while (true){
+    System.out.println("Book title: " );
+    input = keyboard.nextLine();
+
+    if(input.equalsIgnoreCase(str1)){
+        break;
+    }
+
+    books.add(input);
+}
+
+//While loop for removing book titles.
+while (true){
+    System.out.println("Remove a book: " );
+    input = keyboard.nextLine();
+
+    if(input.equalsIgnoreCase(str1)){
+        break;
+    }
+
+    books.remove(input);
+
+}
+
+
+//Display the book titles in the array.
+System.out.println("Your list of book titles: ");
+for (String book : books) {
+    System.out.println(book);
+}
+
+//Sequential search using a while loop
+while(true){
+    
+    System.out.println("Search a title in your database: ");
+String searchInput = keyboard.nextLine();
+
+int index = books.indexOf(searchInput);
+    if (index != -1){
+    System.out.println("Book title: " + searchInput + " found at index: " + index);
+    break;
+} else {
+    System.out.println("Sorry, " + searchInput + " is not in the database.");
+    System.out.println("Try again: ");
+    searchInput = keyboard.nextLine();
+   
+}
+
+
+}
+    
+    }
+
+}
+
+
+
 	public static void main(String[] args) {
 //Fixed size array to store 5 book titles with methods to add, display, and search for books
 		addBook();
@@ -55,4 +135,5 @@ public static void bookSearch() {
 	}
 
 }
+
 
